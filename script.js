78a2d64b3627d6ef3f2fc8a30239f7c4805f13f3
Nvlhnn <hanannaufal17@gmail.com>
2021-01-23 16:09:38 +0700

    basee

---
+++ script.js
@@ -0,0 +1,71 @@
+// let content = document.getElementById('content')
+// let home = document.getElementById('home')
+// let projects = document.getElementById('projects')
+// let skills = document.getElementById('skills')
+// let contact = document.getElementById('contact')
+
+
+// home.addEventListener('click', function () {
+//     content.innerHTML = homeContent()
+
+
+// })
+
+// projects.addEventListener('click', function () {
+//     content.innerHTML = ProjectContent()
+
+// })
+
+// skills.addEventListener('click', function () {
+
+// })
+
+// contact.addEventListener('click', function () {
+
+// })
+
+
+// let homeContent = function () {
+//     return `
+//     <div class="container flex text-center home">
+//     <img src="images/profile.jpeg" width="300" class="rounded-circle">
+
+//     <h1 class="" style="font-size: 700%; margin-top: 5%;">Naufal Hanan</h1>
+//     <p class="" style="font-size: 200%; margin-top: 1%;">Tech <span class="" style="color: #0dac50">
+//             Enthusiast</span> </p>
+
+//     <div class="container list">
+//         <ul class="socmed d-flex justify-content-center flex-row " style="list-style-type: none;">
+//             <li class="ic1"><a href="https://web.facebook.com/naufal.zean"><i class="fa fa-facebook"></i></a>
+//             </li>
+//             <li class="ic2"><a href="https://github.com/nvlhnn"><i class="fa fa-github"></i></a></li>
+//             <li class="ic3"><a href="https://www.instagram.com/nvlhnn/"><i class="fa fa-instagram"></i></a></li>
+//             <li class="ic4"><a href="https://www.linkedin.com/in/naufal-hanan-993584134"><i
+//                         class="fa fa-linkedin"></i></a></li>
+//         </ul>
+//     </div>
+// </div>
+// `
+// }
+
+
+// let ProjectContent = function () {
+//     return `
+// <div class="container flex text-center home " style="height: 800px;">
+
+// <h1 class="" style="font-size: 700%; margin-top: 5%;">Naufal Hanan</h1>
+// <p class="" style="font-size: 200%; margin-top: 1%;">Tech <span class="" style="color: #0dac50">
+//         Enthusiast</span> </p>
+
+// <div class="container list">
+//     <ul class="socmed d-flex justify-content-center flex-row " style="list-style-type: none;">
+//         <li class="ic1"><a href="https://web.facebook.com/naufal.zean"><i class="fa fa-facebook"></i></a>
+//         </li>
+//         <li class="ic2"><a href="https://github.com/nvlhnn"><i class="fa fa-github"></i></a></li>
+//         <li class="ic3"><a href="https://www.instagram.com/nvlhnn/"><i class="fa fa-instagram"></i></a></li>
+//         <li class="ic4"><a href="https://www.linkedin.com/in/naufal-hanan-993584134"><i
+//                     class="fa fa-linkedin"></i></a></li>
+//     </ul>
+// </div>
+// </div>`
+// }
