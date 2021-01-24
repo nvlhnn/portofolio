68a5dea1a767e49f5f9b7916de947eeaaeed69a6
nvlhnn <hanannaufal17@gmail.com>
2021-01-24 14:58:32 +0700

    update anim

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ function create(event) {
     //Set CSS styles so it appears where you clicked (Top left corner)
     i.style.left = event.clientX + 'px';
     i.style.top = event.clientY + 'px';
+    let t = Math.floor((event.clientY + 20) / 67)
+    i.style.animationDuration = t + 's'
 
     //Add it to the body of the document
     document.body.appendChild(i);
-    console.log(i)
+    console.log(t)
 }
 //Main event listener for clicks
 document.addEventListener('click', create);
